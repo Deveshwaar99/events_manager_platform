793ab3c6dc88d89678c24789eba79cd1d155fa58
Deveshwaar99 <deveshwaar99@gmail.com>
2024-01-29 23:09:50 +0530

    configured DB connection and mongoose models

---
+++ src/lib/database/models/category.models.ts
@@ -0,0 +1,18 @@
+import { Document, Schema, model, models } from 'mongoose'
+
+export interface ICategory extends Document {
+  _id: string
+  name: string
+}
+
+const CategorySchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Category name is required'],
+    unique: [true, 'Category name must be unique'],
+  },
+})
+
+const Category = models.Category || model('Category', CategorySchema)
+
+export default Category
---
+++ src/lib/database/models/user.models.ts
@@ -0,0 +1,26 @@
+import { Schema, model, models } from 'mongoose'
+
+const UserSchema = new Schema({
+  clerkId: {
+    type: String,
+    required: [true, 'Clerk ID is required'],
+    unique: [true, 'Clerk ID must be unique'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: [true, 'Email must be unique'],
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: [true, 'Username must be unique'],
+  },
+  firstName: { type: String, required: [true, 'First name is required'] },
+  lastName: { type: String, required: [true, 'Last name is required'] },
+  photo: { type: String, required: [true, 'Photo is required'] },
+})
+
+const User = models.User || model('User', UserSchema)
+
+export default User
