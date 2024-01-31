29827b40e742c342c9f2c704d0a04a4ec3a6f53b
Deveshwaar99 <deveshwaar99@gmail.com>
2024-01-31 14:58:52 +0530

    added error handler function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function handleError(error: unknown) {
+  console.log(error)
+  if (typeof error === 'string') {
+    throw new Error(error)
+  } else {
+    throw new Error(JSON.stringify(error))
+  }
 }
