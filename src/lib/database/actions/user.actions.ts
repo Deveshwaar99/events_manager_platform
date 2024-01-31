f0c2a860e30f1334853cb40f2dad1b5041a9cf92
Deveshwaar99 <deveshwaar99@gmail.com>
2024-01-31 21:28:17 +0530

    Add user retrieval (getUserById) and update functionality (updateUser)
    - Implement `getUserById` function to retrieve users by ID.
    - Add `updateUser` functionality to modify user information.
    - Integrate with Events model (commented out `deleteUser` for future).
    - Update `CreateUserParams` and `UserUpdateProperties` types.

--- src/lib/database/actions/user.actions.ts
+++ src/lib/database/actions/user.actions.ts
@@ -4,6 +4,9 @@ import { handleError } from '@/lib/utils'
 import { connectToDatabase } from '../index'
 //models
 import User from '../models/user.models'
+import Event from '../models/events.models'
+
+import { NextResponse } from 'next/server'
 
 export type CreateUserParams = {
   clerkId: string
@@ -13,6 +16,12 @@ export type CreateUserParams = {
   email: string
   photo: string
 }
+export type UserUpdateProperties = {
+  firstName: string
+  lastName: string
+  username: string
+  photo: string
+}
 
 export async function createUser(user: CreateUserParams) {
   try {
@@ -20,6 +29,42 @@ export async function createUser(user: CreateUserParams) {
     const newUser = await User.create(user)
 
     return JSON.parse(JSON.stringify(newUser))
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+export async function getUserById(id: string) {
+  try {
+    await connectToDatabase()
+    const user = await User.findById(id)
+    if (!user) {
+      throw new Error('User not found')
+    }
+    return JSON.parse(JSON.stringify(user))
+  } catch (error) {
+    handleError(error)
+  }
+}
+// export async function deleteUser(clerkId: string) {
+//   try {
+//     await connectToDatabase()
+//     const user = await User.findOne({ clerkId })
+//     if (!user) {
+//       throw new Error('User not found')
+//     }
+//     await Event.updateMany({ _id: { $in: user.events } },{$pull:})
+//   } catch (error) {
+//     handleError(error)
+//   }
+// }
+export async function updateUser(clerkId: string, updateProperties: UserUpdateProperties) {
+  try {
+    let updatedUser = await User.findOneAndUpdate({ clerkId }, updateProperties, { new: true })
+    if (!updatedUser) {
+      throw new Error('User not found')
+    }
+    return JSON.parse(JSON.stringify(updatedUser))
   } catch (error) {
     handleError(error)
   }
