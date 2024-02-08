536d332e51a1e6dcb6030f3ec7d8fa5bf6bfc454
Deveshwaar99 <deveshwaar99@gmail.com>
2024-02-08 16:17:07 +0530

    non-working code

---
+++ src/lib/stripe.actions.ts
@@ -0,0 +1,45 @@
+'use server'
+
+import Stripe from 'stripe'
+import { handleError } from './utils'
+import { redirect } from 'next/navigation'
+
+type CheckoutOrderParams = {
+  buyerId: string
+  eventId: string
+  eventTitle: string
+  price: string
+  isFree: boolean
+}
+
+export async function checkoutOrder(order: CheckoutOrderParams) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+
+  const price = order.isFree ? 0 : Number(order.price) * 100
+
+  try {
+    // Create Checkout Sessions from body params.
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            unit_amount: price,
+            product_data: {
+              name: order.eventTitle,
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      metadata: { eventId: order.eventId, buyerId: order.buyerId },
+      mode: 'payment',
+      success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
+      cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
+    })
+
+    redirect(session.url!)
+  } catch (err) {
+    handleError(err)
+  }
+}
