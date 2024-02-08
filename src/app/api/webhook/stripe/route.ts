7f94a3b2ae72ab80ff48489adc071c52c8504055
Deveshwaar99 <deveshwaar99@gmail.com>
2024-02-08 22:43:44 +0530

    fixiing stripe

--- src/app/api/webhook/stripe/route.ts
+++ src/app/api/webhook/stripe/route.ts
@@ -3,14 +3,14 @@ import { NextRequest, NextResponse } from 'next/server'
 import stripe from 'stripe'
 
 export async function POST(request: Request) {
-  const body = await request.json()
+  const body = await request.text()
 
   const sig = request.headers.get('stripe-signature') as string
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!
   let event
 
   try {
-    console.log(sig, endpointSecret)
+    console.log({ sig, endpointSecret })
     event = stripe.webhooks.constructEvent(body, sig, endpointSecret)
     console.log('Event recieved', event)
   } catch (err) {
